65d404598dd398d7538c51f1ff131123fe3e4b94
Kavin <kavinmadusanka20011@gmail.com>
2026-04-23 17:36:39 +0530

    Initial set up for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+
+import Homepage from './pages/Homepage'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
